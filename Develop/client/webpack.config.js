9adf7cdf4d8fa0d7bff9e3f1c57825f15f22d83f
Christopher Vazquez <cvg_v10@hotmail.com>
2022-05-07 22:27:58 -0700

    ran modules for rules

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -44,5 +44,80 @@ module.exports = () => {
         ],
       })
     ],
+     module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+      ],
+    },
+
+     module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['no style', 'no style'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+      ],
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   };
 };

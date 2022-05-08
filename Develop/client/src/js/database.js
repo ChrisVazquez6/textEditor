435b4bca33ce65bee37c094e7daffbb29c8cecd6
Christopher Vazquez <cvg_v10@hotmail.com>
2022-05-07 22:34:19 -0700

    added logic to get the data bases for jate and objects

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -29,5 +29,11 @@ const tx = jateDb.transaction('jate', 'readwrite');
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
+const jateDb = await openDB('jate', 1);
+const tx = jateDb.transaction('jate', 'readonly');
+const store = tx.objectStore('jate');
+const request = store.get(1);
+const result = await request;
+result
 
 initdb();
